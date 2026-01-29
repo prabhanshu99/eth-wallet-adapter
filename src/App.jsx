87f845cff0b1fb3a2867dcafbf7786fc971c0e4f
Prabhanshu <prabhanshuop@gmail.com>
2026-01-30 00:32:43 +0530

    need to fix this using wagmi docs

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,69 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {QueryClient, QueryClientProvider, useQuery, useQueryClient} from "@tanstack/react-query"
 import './App.css'
+import { http, createConfig, useConnect, WagmiProvider, useAccount, useSendTransaction, useBalance, useConnectors } from 'wagmi'
+import { base, mainnet, optimism } from 'wagmi/chains'
+import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors'
+
+
+const queryClient = new QueryClient()
+
+
+export const config = createConfig({
+  chains: [mainnet],
+  connectors: [
+    injected()
+  ],
+  transports: {
+    [mainnet.id]: http()
+  },
+})
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}> 
+        <WalletConnector/>
+        <EthSend/>
+        <MyAddress/>
+      </QueryClientProvider> 
+    </WagmiProvider>
   )
+}
+
+function MyAddress(){
+  const address = useBalance({
+    address: document.getElementById("address")
+  })
+}
+
+function sendEth(){
+  const sendTransaction = useSendTransaction();
+
+  sendTransaction.mutate({
+    to: document.getElementById("address"),
+    value: parseEther('0.01'),
+  })
+}
+
+function EthSend(){
+  return <div>
+    <input id='address'></input>
+    <button onClick={sendEth}>Send Eth</button>
+  </div>
+}
+
+function WalletConnector(){
+  const connect = useConnect();
+  const connectors = useConnectors();
+
+  return connectors.map((connector) => (
+    <button key={connector.uid} onClick={() => connect({ connector })}>
+      {connector.name}
+    </button>
+  ))
 }
 
 export default App
