613d3810914b2baa175109c4e79dd8fa35ec8366
Prabhanshu <prabhanshuop@gmail.com>
2026-01-30 22:54:39 +0530

    added transaction and fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
-import {QueryClient, QueryClientProvider, useQuery, useQueryClient} from "@tanstack/react-query"
+import { QueryClient, QueryClientProvider, useQuery, useQueryClient } from "@tanstack/react-query"
 import './App.css'
 import { http, createConfig, useConnect, WagmiProvider, useAccount, useSendTransaction, useBalance, useConnectors } from 'wagmi'
 import { base, mainnet, optimism } from 'wagmi/chains'
 import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors'
+import { connect } from 'wagmi/actions'
+import { parseEther } from 'viem'
 
 
 const queryClient = new QueryClient()
@@ -23,44 +25,40 @@ export const config = createConfig({
 function App() {
 
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}> 
-        <WalletConnector/>
-        <EthSend/>
-        <MyAddress/>
-      </QueryClientProvider> 
-    </WagmiProvider>
+    <QueryClientProvider client={queryClient}>
+      <WagmiProvider config={config}>
+        <WalletConnector />
+        <EthSend />
+      </WagmiProvider>
+    </QueryClientProvider>
   )
 }
 
-function MyAddress(){
-  const address = useBalance({
-    address: document.getElementById("address")
-  })
-}
 
-function sendEth(){
-  const sendTransaction = useSendTransaction();
+function EthSend() {
+  const [address, setAddress] = useState('')
+  const sendTransaction = useSendTransaction()
 
-  sendTransaction.mutate({
-    to: document.getElementById("address"),
-    value: parseEther('0.01'),
-  })
-}
+  function sendEth() {
 
-function EthSend(){
+    sendTransaction.mutate({
+      to: address,
+      value: parseEther('0.01'),
+    })
+  }
   return <div>
-    <input id='address'></input>
+    <input value={address} onChange={(e) => {
+      setAddress(e.target.value)
+    }}></input>
     <button onClick={sendEth}>Send Eth</button>
   </div>
 }
 
-function WalletConnector(){
-  const connect = useConnect();
-  const connectors = useConnectors();
+export function WalletConnector() {
+  const connectors = useConnectors()
 
   return connectors.map((connector) => (
-    <button key={connector.uid} onClick={() => connect({ connector })}>
+    <button key={connector.uid} onClick={() => connect(config, { connector })}>
       {connector.name}
     </button>
   ))
